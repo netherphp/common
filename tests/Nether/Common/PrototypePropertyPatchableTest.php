604f8a206455a51fcc599f388194efe421114234
bobmagicii <bob@majdak.net>
2023-02-16 00:54:14 +0000

    thusfaR

---
+++ tests/Nether/Common/PrototypePropertyPatchableTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Nether\Common;
+
+use PHPUnit\Framework\TestCase;
+
+class PatchableTest1
+extends Prototype {
+
+	public int $ID;
+
+	#[Meta\PropertyPatchable]
+	#[Meta\PropertyFilter([ 'Nether\\Common\\Datafilters', 'TrimmedText' ])]
+	public string $Name;
+
+	#[Meta\PropertyPatchable]
+	#[Meta\PropertyFilter([ 'Nether\\Common\\Datafilters', 'TrimmedText' ])]
+	#[Meta\PropertyFilter([ 'Nether\\Common\\Datafilters', 'StrippedText' ])]
+	public string $Food;
+
+}
+
+class PrototypePropertyPatchableTest
+extends TestCase {
+
+	/** @test */
+	public function
+	TestBasic():
+	void {
+
+
+		$Props = PatchableTest1::GetPropertyIndex();
+
+		$Patchable = PatchableTest1::GetPropertiesWithAttribute(
+			Meta\PropertyPatchable::class
+		);
+
+		$this->AssertCount(3, $Props);
+		$this->AssertCount(2, $Patchable);
+
+		////////
+
+		$Prop = NULL;
+		$Filters = NULL;
+		$Filter = NULL;
+		$Value = NULL;
+
+		foreach($Patchable as $Prop) {
+			$Filters = $Prop->GetAttributes(Meta\PropertyFilter::class);
+
+			if($Prop->Name === 'Name') {
+				$this->AssertCount(1, $Filters);
+				$Value = ' Bob ';
+
+				foreach($Filters as $Filter)
+				$Value = $Filter($Value);
+
+				$this->AssertEquals('Bob', $Value);
+			}
+
+			if($Prop->Name === 'Food') {
+				$this->AssertCount(2, $Filters);
+				$Value = ' <b>Bob</b> ';
+
+				foreach($Filters as $Filter)
+				$Value = $Filter($Value);
+
+				$this->AssertEquals('Bob', $Value);
+			}
+		}
+
+		return;
+	}
+
+
+}
