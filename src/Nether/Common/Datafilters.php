e657846df91548e34f5c62f645eabd238e015721
bob <bob@tech.live>
2023-03-07 21:01:43 +0000

    add social media formatters

--- src/Nether/Common/Datafilters.php
+++ src/Nether/Common/Datafilters.php
@@ -264,6 +264,121 @@ class Datafilters {
 		)) ?: NULL;
 	}
 
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	static public function
+	WebsiteURL($Val):
+	string {
+
+		static::Prepare($Val);
+
+		$Val = trim(strip_tags($Val));
+		if(!$Val) return '';
+
+		if(!preg_match('/^https?:\/\//',$Val))
+		return "http://{$Val}";
+
+		return $Val;
+	}
+
+	static public function
+	FacebookURL($Val):
+	string {
+
+		static::Prepare($Val);
+
+		$Val = trim(strip_tags($Val));
+		if(!$Val) return '';
+
+		if(strpos($Val,'/') === FALSE)
+		return sprintf(
+			'https://facebook.com/%s',
+			ltrim($Val,'@')
+		);
+
+		return static::WebsiteURL($Val);
+	}
+
+	static public function
+	TwitterURL($Val):
+	string {
+
+		static::Prepare($Val);
+
+		$Val = trim(strip_tags($Val));
+		if(!$Val) return '';
+
+		if(strpos($Val,'/') === FALSE)
+		return sprintf(
+			'https://twitter.com/%s',
+			ltrim($Val,'@')
+		);
+
+		return static::WebsiteURL($Val);
+	}
+
+	static public function
+	InstagramURL($Val):
+	string {
+
+		static::Prepare($Val);
+
+		$Val = trim(strip_tags($Val));
+		if(!$Val) return '';
+
+		if(strpos($Val,'/') === FALSE)
+		return sprintf(
+			'https://instagram.com/%s',
+			ltrim($Val,'@')
+		);
+
+		return static::WebsiteURL($Val);
+	}
+
+	static public function
+	TikTokURL($Val):
+	string {
+
+		static::Prepare($Val);
+
+		$Val = trim(strip_tags($Val));
+		if(!$Val) return '';
+
+		if(strpos($Val,'/') === FALSE) {
+			if(!str_starts_with($Val, '@'))
+			$Val = "@{$Val}";
+
+			return sprintf(
+				'https://tiktok.com/%s',
+				$Val
+			);
+		}
+
+		return static::WebsiteURL($Val);
+	}
+
+	static public function
+	YouTubeURL($Val):
+	string {
+
+		static::Prepare($Val);
+
+		$Val = trim(strip_tags($Val));
+		if(!$Val) return '';
+
+		if(strpos($Val,'/') === FALSE)
+		return sprintf(
+			'https://youtube.com/channel/%s',
+			ltrim($Val,'@')
+		);
+
+		return static::WebsiteURL($Val);
+	}
+
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
 	static public function
 	UUID(mixed $Item):
 	?string {
