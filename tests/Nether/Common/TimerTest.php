000fb6d89748c1b18979d2f2dfc9c6657e1de553
bobmagicii <bob@majdak.net>
2023-04-26 19:15:16 +0000

    faster and more test for timer

--- tests/Nether/Common/TimerTest.php
+++ tests/Nether/Common/TimerTest.php
@@ -8,10 +8,18 @@ class TimerTest
 extends TestCase {
 
 	public function
-	ThingToDo(float $Time=1.1):
+	ThingToDo(float $Time=0.1):
 	void {
 
 		usleep($Time * 1000000);
+		return;
+	}
+
+	public function
+	OtherThingToDo(float $Time=0.2):
+	void {
+
+		$this->ThingToDo($Time);
 		return;
 	}
 
@@ -23,27 +31,39 @@ extends TestCase {
 		$Timer = new Timer($this->ThingToDo(...));
 
 		// run a timer.
+
 		$Timer->Run();
-		$this->AssertGreaterThan(1.0, $Timer->Time);
+		$this->AssertGreaterThan(0.1, $Timer->Time);
+		$this->AssertLessThan(0.2, $Timer->Time);
 
 		// accumulate more time on the same timer.
+
 		$Timer->Run();
-		$this->AssertGreaterThan(2.0, $Timer->Time);
+		$this->AssertGreaterThan(0.2, $Timer->Time);
+		$this->AssertLessThan(0.3, $Timer->Time);
 
 		// reset the timer.
+
 		$Timer->Reset();
 		$this->AssertEquals(0.0, $Timer->Time);
 
 		// run timer with a passed argument.
+
 		$Timer->Run(0.2);
 		$this->AssertGreaterThan(0.2, $Timer->Time);
-		$this->AssertLessThan(1.0, $Timer->Time);
+		$this->AssertLessThan(0.3, $Timer->Time);
 
 		// run timer via invoke and passed arg.
+
 		$Timer->Reset();
-		$Time = $Timer(NULL, 0.3);
-		$this->AssertGreaterThan(0.3, $Time);
-		$this->AssertLessThan(1.0, $Time);
+		$Time = $Timer(NULL, 0.2);
+		$this->AssertGreaterThan(0.2, $Time);
+		$this->AssertLessThan(0.3, $Time);
+
+		$Timer->Reset();
+		$Time = $Timer($this->OtherThingToDo(...), 0.2);
+		$this->AssertGreaterThan(0.2, $Time);
+		$this->AssertLessThan(0.3, $Time);
 
 		return;
 	}
