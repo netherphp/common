add6a2c8d0da7587091ad08c7537645b92d108af
bobmagicii <bob@majdak.net>
2023-08-13 18:03:24 -0500

    thusfar

--- tests/Cron/CrontabEntryTest.php
+++ tests/Cron/CrontabEntryTest.php
@@ -73,7 +73,7 @@ extends TestCase {
 
 		$Line = Common\Struct\CrontabEntry::FromCrontab(sprintf(
 			'%d %d * * * minutely',
-			($Future->Get('i')-1), $Future->Get('H')-1)
+			($Future->Get('i')-1), ($Future->Get('H')-1)
 		));
 
 		$Date = $Line->GetTimerAsObject();
