ab63b768461b9301b4d5aeefd49a17994dadd0b4
bobmagicii <bob@majdak.net>
2023-08-13 04:05:37 -0500

    thusfar

--- tests/Filesystem/MiscTest.php
+++ tests/Filesystem/MiscTest.php
@@ -21,15 +21,22 @@ extends TestCase {
 	TestDirectoryClass():
 	void {
 
-		$Link = new Common\Filesystem\Directory([
-			'Path'   => '/path',
-			'Mode'   => 0o755
+		$Dir = new Common\Filesystem\Directory([
+			'Path' => '/path',
+			'Mode' => 0o755
 		]);
 
-		$this->AssertEquals('/path', $Link->Path);
-		$this->AssertEquals(0o755, $Link->Mode);
+		$Dir2 = new Common\Filesystem\Directory([
+			'Path' => '/path',
+			'Mode' => '0o755'
+		]);
 
-		$Array = json_decode(json_encode($Link), TRUE);
+
+		$this->AssertEquals('/path', $Dir->Path);
+		$this->AssertEquals(0o755, $Dir->Mode);
+		$this->AssertEquals(0o755, $Dir2->Mode);
+
+		$Array = json_decode(json_encode($Dir), TRUE);
 		$this->AssertTrue(isset($Array['Path']));
 		$this->AssertTrue(isset($Array['Mode']));
 
@@ -47,9 +54,16 @@ extends TestCase {
 			'Mode'   => 0o755
 		]);
 
+		$Link2 = new Common\Filesystem\Symlink([
+			'Path'   => '/path',
+			'Source' => '/source',
+			'Mode'   => '0o755'
+		]);
+
 		$this->AssertEquals('/path', $Link->Path);
 		$this->AssertEquals('/source', $Link->Source);
 		$this->AssertEquals(0o755, $Link->Mode);
+		$this->AssertEquals(0o755, $Link2->Mode);
 
 		$Array = json_decode(json_encode($Link), TRUE);
 		$this->AssertTrue(isset($Array['Path']));
