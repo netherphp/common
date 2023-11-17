f75b9ce196284e9aabc229c1e37fbbec0cf7b51a
bobmagicii <bob@majdak.net>
2023-11-16 22:55:09 -0600

    thusfar

--- src/Nether/Common/Error/FileNotFound.php
+++ src/Nether/Common/Error/FileNotFound.php
@@ -2,15 +2,22 @@
 
 namespace Nether\Common\Error;
 
-use Exception;
+use Nether\Common;
 
+#[Common\Meta\Date('2023-11-16')]
 class FileNotFound
-extends Exception {
+extends Common\Error {
+
+	public readonly string
+	$Filename;
 
 	public function
 	__Construct(string $Filename) {
-		parent::__Construct("file {$Filename} not found");
+		parent::__Construct("file not found: {$Filename}");
+
+		$this->Filename = $Filename;
+
 		return;
 	}
 
-}
+};
