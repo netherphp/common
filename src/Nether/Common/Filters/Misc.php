c52cb78190292834bb6d1d65d92f2778cec28e28
bob <bob@tech.live>
2023-11-23 08:16:36 +0000

    thusfar

--- src/Nether/Common/Filters/Misc.php
+++ src/Nether/Common/Filters/Misc.php
@@ -25,4 +25,49 @@ class Misc {
 		return $Item ?: NULL;
 	}
 
+	////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////
+
+	#[Common\Meta\Date('2023-11-22')]
+	#[COmmon\Meta\Info('Input must be one of these else shoot back this other thing.')]
+	static public function
+	OneOfTheseOr(mixed $Input, array $Valids=[], mixed $Default=NULL):
+	mixed {
+
+		static::Prepare($Input);
+
+		////////
+
+		if(in_array($Input, $Valids, TRUE))
+		return $Input;
+
+		////////
+
+		return $Default;
+	}
+
+	#[Common\Meta\Date('2023-11-22')]
+	#[Common\Meta\Info('Input must be one of these else shoots back the first one.')]
+	static public function
+	OneOfTheseFirst(mixed $Input, array $Valids=[]):
+	mixed {
+
+		return static::OneOfTheseOr(
+			$Input, $Valids,
+			$Valids[array_key_first($Valids)]
+		);
+	}
+
+	#[Common\Meta\Date('2023-11-22')]
+	#[COmmon\Meta\Info('The input must be one of these else shoots back NULL.')]
+	static public function
+	OneOfTheseNullable(mixed $Input, array $Valids=[]):
+	mixed {
+
+		return static::OneOfTheseOr(
+			$Input, $Valids,
+			NULL
+		);
+	}
+
 }
