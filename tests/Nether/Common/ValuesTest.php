2f93e43b067188fd3e58a8873d54b4e8f9903006
bob <bob@tech.live>
2024-04-30 01:21:57 +0000

    tests for If*

--- tests/Nether/Common/ValuesTest.php
+++ tests/Nether/Common/ValuesTest.php
@@ -21,5 +21,68 @@ extends TestCase {
 		return;
 	}
 
+	/** @test */
+	public function
+	TestIfOneElse():
+	void {
+
+		$Dataset = [
+			[ [ -1, 'one', 'not' ], 'not' ],
+			[ [  0, 'one', 'not' ], 'not' ],
+			[ [  1, 'one', 'not' ], 'one' ],
+			[ [  2, 'one', 'not' ], 'not' ]
+		];
+
+		////////
+
+		$Input = NULL;
+
+		foreach($Dataset as $Input)
+		$this->AssertEquals(
+			Values::IfOneElse(...$Input[0]),
+			$Input[1]
+		);
+
+		return;
+	}
+
+	/** @test */
+	public function
+	TestIfTrueElse():
+	void {
+
+		$Dataset = [
+
+			// plausable inputs.
+			[ [ TRUE,  'yeh', 'nah' ], 'yeh' ],
+			[ [ FALSE, 'yeh', 'nah' ], 'nah' ],
+			[ [ 1,     'yeh', 'nah' ], 'yeh' ],
+			[ [ 0,     'yeh', 'nah' ], 'nah' ],
+
+			// things that typecast false.
+			[ [ '0',   'yeh', 'nah' ], 'nah' ],
+			[ [ '',    'yeh', 'nah' ], 'nah' ],
+
+			// things that typecast true.
+			[ [ '1',   'yeh', 'nah' ], 'yeh' ],
+			[ [ 'wat', 'yeh', 'nah' ], 'yeh' ],
+			[ [ '   ', 'yeh', 'nah' ], 'yeh' ],
+			[ [ 'y',   'yeh', 'nah' ], 'yeh' ],
+			[ [ 'n',   'yeh', 'nah' ], 'yeh' ]
+
+		];
+
+		////////
+
+		$Input = NULL;
+
+		foreach($Dataset as $Input)
+		$this->AssertEquals(
+			Values::IfTrueElse(...$Input[0]),
+			$Input[1]
+		);
+
+		return;
+	}
 
 }
