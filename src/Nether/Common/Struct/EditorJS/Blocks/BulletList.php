dda69ae53ac49d15e9c8f4ef4edee4cbfbcc7ecf
bobmagicii <bob@majdak.net>
2024-06-24 06:24:46 +0000

    thusfar

---
+++ src/Nether/Common/Struct/EditorJS/Blocks/BulletList.php
@@ -0,0 +1,67 @@
+<?php ##########################################################################
+################################################################################
+
+namespace Nether\Common\Struct\EditorJS\Blocks;
+
+use Nether\Common;
+
+################################################################################
+################################################################################
+
+class BulletList
+extends Common\Struct\EditorJS\Block {
+
+	protected function
+	OnReady(Common\Prototype\ConstructArgs $Raw):
+	void {
+
+		parent::OnReady($Raw);
+
+		($this->Data)
+		->Text(Common\Filters\Text::Trimmed(...))
+		->Caption(Common\Filters\Text::Trimmed(...));
+
+		return;
+	}
+
+	public function
+	__ToString():
+	string {
+
+		$Item = NULL;
+		$Output = '';
+
+		foreach($this->Data->Items as $Item)
+		$Output .= $this->GenerateListLevel($Item);
+
+		return "<ul>{$Output}</ul>";
+	}
+
+	public function
+	GenerateListLevel($Item):
+	string {
+
+		$Nested = '';
+		$Output = '';
+		$Sub = NULL;
+
+		////////
+
+		if(count($Item->items)) {
+			foreach($Item->items as $Sub)
+			$Nested .= sprintf(
+				'<ul>%s</ul>',
+				$this->GenerateListLevel($Sub)
+			);
+		}
+
+		$Output = sprintf(
+			'<li>%s%s</li>',
+			$Item->content,
+			$Nested ?: ''
+		);
+
+		return $Output;
+	}
+
+}
